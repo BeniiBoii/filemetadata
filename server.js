a8b597ea868ec81e17379ed80bfd3e75fd3b4275
Benjamin Netanyahu <beniiboii00@gmail.com>
2021-10-09 22:10:10 +1100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+require('dotenv').config();
+
+const upload = multer({ dest: 'public/files' });
 
 var app = express();
 
@@ -8,13 +12,15 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const { originalname, mimetype, size } = req.file;
+  res.send({ name: originalname, type: mimetype, size });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
